4ab6c83904c1fccec506e3c4a6687b0e91c2a704
VanessaLiaw021 <vliaw021@gmail.com>
2022-05-31 19:58:47 -0400

    create a function that will append user input to li element and stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,4 +16,33 @@ $(document).ready(function() {
         //Append the li element to the ul element
         ulElement.append(createLiElement);
     }
+
+    //Function that read the user input and stored it in local storage 
+    function handleSubmitButton() {
+
+        //Get the value of the user input 
+        var userInput = $("#search-text").val();
+
+        //Call the function recent searches to append it to the li element 
+        appendElement(userInput);
+
+        //Store the user cities input into an object 
+        var searches = {
+            cities: userInput
+        }
+
+        //Get the item in local storage and if not there create an empty array 
+        var cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+
+        //Push the array if not in local storage 
+        cityList.push(searches);
+
+        //Set the key and value in local storage 
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+    }
+
+    //Function that will get the item from local storage and save it on the html page 
+    function displayFromLocalStorage() {
+
+    }
 });
